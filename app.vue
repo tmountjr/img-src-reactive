68d6a148efdf5953e2a4060aa45c60a7dfbd2c03
Tom Mount <tmountjr@gmail.com>
2023-02-22 14:32:06 -0500

    style cleanups

--- app.vue
+++ app.vue
@@ -2,8 +2,10 @@
   <div>
     <h1>With Vuetify</h1>
     <VuetifyImageProvider />
+    <hr />
     <h1>With Vuetify, Simple Image Provider</h1>
     <VuetifyImageProvider simple />
+    <hr />
     <h1>Without Veutify</h1>
     <HtmlImageProvider />
   </div>
